5e4a2b9bc6f89449462c0f72a08912623813514e
Loveday-Alfred <64823301+Loveday-Alfred@users.noreply.github.com>
2020-05-20 10:03:16 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+$(document).ready(function(e){
+    $win = $(window);
+    $navbar = $('#header');
+    $toggle = $('.toggle-button');
+    var width = $navbar.width();
+    toggle_onclick($win,$navbar,width);
+
+    // resize event
+    $win.resize(function(){
+        toggle_onclick($win,$navbar,width);
+    });
+
+    $toggle.click(function(e){
+        $navbar.toggleClass("toggle-left");
+    })
+    
+});
+
+function toggle_onclick($win,$navbar,width){
+    if ($win.width()<=728) {
+        $navbar.css({left:`-${width}px`});
+    }else{
+        $navbar.css({left:'opx'});
+    }
+}
